8462dfa08817fff59ade25b477aec11ca4916d31
mattseb <mateosmv1608@gmail.com>
2022-10-27 16:49:40 -0500

    cambios actividad jeuves

--- app/src/main/java/com/example/miprimeraaplicacion/MainActivity.kt
+++ app/src/main/java/com/example/miprimeraaplicacion/MainActivity.kt
@@ -7,5 +7,84 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        validarMayorEdad(18)
+        multiplicarN(5)
+        listadoParalelo()
+        val carro = Carro()
+        carro.imprimir()
+        calcularIVA(50)
+    }
+
+    fun validarMayorEdad(edad: Int){
+        if(edad < 18){
+            println("Usted es menor de edad")
+        }else{
+            println("Ustde es mayor de edad")
+
+        }
+    }
+
+    fun multiplicarN(n: Int){
+        for (i in 1..10){
+            println(i * n)
+        }
+
+        println();
+
+        for (i in 10 downTo 1)
+            println(i * n)
+    }
+
+    fun listadoParalelo(){
+        var listadoEstudiantes = listOf<String>("Persona 1",
+            "Persona 2",
+            "Persona 3",
+            "Persona 4",
+            "Persona 5",)
+
+        listadoEstudiantes.forEach {
+            println(it)
+        }
+
+        val subGrupos: Map<String, String> = mapOf(
+            "Martinez Mateo " to "Mascotas",
+            "Alvarado Eric " to "Registro UTPL",
+            "Leiva Diego" to "Mascotas",
+            "Cuenca Erick " to "Registro UTPL",
+            "Ojeda Rommel " to "Registro UTPL",
+            "Caraguay Miguel" to "Mascotas",
+        )
+
+        val ordenado = subGrupos.toSortedMap()
+        println(ordenado)
+    }
+
+
+
+    class Carro {
+
+        private var traccion: String = "Buena"
+        private var motor: String = "Buena"
+        private var tipoVehiculo: String = "Buena"
+        private var capacidad: Int = 5
+
+
+        fun imprimir() {
+            println("Traccion:" + traccion)
+            println("Motor:" + motor)
+            println("Tipo Vehiculo:" + tipoVehiculo)
+            println("Capacidad:" + capacidad)
+        }
+    }
+
+
+    fun calcularIVA(valor: Int){
+        println("El iva del valor " + valor + " es de " + (valor*0.12))
+    }
+
+    fun validarCedula(cedula: Int){
+        var valores = listOf<Int>(2,1,2,1,2,1,2,1,2)
     }
 }
+
